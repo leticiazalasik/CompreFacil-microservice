db4b176870d36dc386b9396405050bc495ca93a2
Leticia Zalasik <leticiazalasik@gmail.com>
2026-04-22 10:11:28 -0300

    Adicionadas e aplciadas as funções para simular o processamento de pagamento, confirmação de pagamento e notificar o sucesso do pagamento;

--- payment-service/controllers/transactions.controller.js
+++ payment-service/controllers/transactions.controller.js
@@ -10,14 +10,24 @@ class TransactionController {
 
     try {
       //Armazena a transação com status pendente
-      let transacao = await ts.salvaTransacao(usuario,valor)
+      let transacao = await ts.salvaTransacao(usuario, valor);
       console.log("Transação salva:", transacao);
 
       //Publica mensagem na fila para o serviço de notificação
       ts.notificarTransacao(transacao);
 
+      // Simula processamento do pagamento
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+
+      // confirma pagamento
+      transacao = await ts.confirmaTransacao(transacao.id);
+
+      //Notificar o sucesso do pagamento
+      ts.notificarPagamentoConfirmado(transacao);
+      console.log("Pagamento confirmado:", transacao);
+
       res.status(201).json({
-        mensagem: "Transação recebida com status pendente",
+        mensagem: "Transação criada e confirmada",
         transacao,
       });
     } catch (err) {
